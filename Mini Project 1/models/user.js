32157ad074fae6fe054fd208e31fe08f7be66083
Subham <subhambwn137@gmail.com>
2026-05-08 21:52:05 +0530

    Add comments to user schema in user.js
    Added comments to clarify the purpose of the user schema.

--- Mini Project 1/models/user.js
+++ Mini Project 1/models/user.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose')
 
 mongoose.connect("mongodb://127.0.0.1:27017/miniproject")
 
+//User Schema
 let userSchema=mongoose.Schema({
     username:String,
     name:String,
