57b9cbbd6118df0d3ca0dc9b491fcb026cc6ae5a
Subham <subhambwn137@gmail.com>
2026-05-08 21:51:33 +0530

    Update post.js

--- Mini Project 1/models/post.js
+++ Mini Project 1/models/post.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose')
 
 mongoose.connect("mongodb://127.0.0.1:27017/miniproject")
 
+// Post Schema
 let postSchema=mongoose.Schema({
     user:{ 
     type:mongoose.Schema.Types.ObjectId,
